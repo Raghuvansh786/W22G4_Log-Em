be6a1b5065dedadd282686bf2abf3345840875fe
Raghuvansh786 <girir1@student.douglascollege.ca>
2022-04-05 23:31:16 -0700

    Showed the Employee Schedule to the Employee in a custom calender View.

--- app/src/main/java/com/example/log_em/LandingPage.java
+++ app/src/main/java/com/example/log_em/LandingPage.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.log_em.databinding.ActivityLandingPageBinding;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -23,18 +29,25 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class LandingPage extends AppCompatActivity {
 
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+    TextView txtViewMonth;
+    ImageView imgViewNext, imgViewBack;
     Button btnLogOut;
     ActivityLandingPageBinding binding;
-    CalendarView calendarViewSchedule;
+    CompactCalendarView compactCalendar;
     private static final String TAG = "Landing";
+    SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,21 +59,78 @@ public class LandingPage extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         btnLogOut = binding.btnLogOut;
-        calendarViewSchedule = binding.calendarViewSchedule;
+        compactCalendar = binding.compactCalendarSchedule;
+        txtViewMonth =binding.txtViewMonth;
+        imgViewBack = binding.imgViewPrev;
+        imgViewNext = binding.imgViewNext;
+        txtViewMonth.setText(dateFormatForMonth.format(compactCalendar.getFirstDayOfCurrentMonth()));
 
-        Log.d("NEW","Entered in the new activity"+ TAG);
 
+        Log.d("NEW", "Entered in the new activity" + TAG);
 
 
         // Getting the Current User id
         String userID = fAuth.getCurrentUser().getUid();
         Log.d(TAG, "onCreate: Current User ID:  " + userID);
 
-        getSchedule(userID);
+        compactCalendar.shouldSelectFirstDayOfMonthOnScroll(false);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
 
+//        getSchedule(userID);
 
+        DocumentReference df = fStore.collection("Users").document(userID);
 
-        btnLogOut.setOnClickListener((View view) ->{
+        df.get().addOnCompleteListener(
+                (@NonNull Task<DocumentSnapshot> task) -> {
+
+                    Log.d("abcd", "onComplete: Reading From the database");
+                    DocumentSnapshot document = task.getResult();
+                    List<String> schedule = (List<String>) document.get("schedule");
+                    Log.d("list", "onComplete: Schedule List " + schedule);
+                    Log.d(TAG, "getSchedule: Schedule List Size: " + schedule.size());
+
+                    for (int i = 0; i < schedule.size(); i++) {
+
+                        String[] parts = schedule.get(i).split("/");
+
+                        int month = Integer.parseInt(parts[0]);
+                        int day = Integer.parseInt(parts[1]);
+                        int year = Integer.parseInt(parts[2]);
+
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, (month-1));
+                        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+                        long milliTime = calendar.getTimeInMillis();
+                        Log.d(TAG, "onCreate: " + milliTime);
+
+                        Event ev1 = new Event(Color.GREEN,milliTime);
+                        compactCalendar.addEvent(ev1);
+//                        String x = String.valueOf(calendarViewSchedule.getDate());
+
+//                        Log.d(TAG, "onCreate: Current date from calendar view " + x);
+
+                    }
+                });
+
+        imgViewNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                compactCalendar.scrollRight();
+                txtViewMonth.setText(dateFormatForMonth.format(compactCalendar.getFirstDayOfCurrentMonth()));
+            }
+        });
+
+        imgViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                compactCalendar.scrollLeft();
+                txtViewMonth.setText(dateFormatForMonth.format(compactCalendar.getFirstDayOfCurrentMonth()));
+            }
+        });
+
+        btnLogOut.setOnClickListener((View view) -> {
 
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(this, LogInActivity.class));
@@ -70,36 +140,59 @@ public class LandingPage extends AppCompatActivity {
     }
 
     private void getSchedule(String uid) {
+
         DocumentReference df = fStore.collection("Users").document(uid);
 
         df.get().addOnCompleteListener(
                 (@NonNull Task<DocumentSnapshot> task) -> {
 
-                Log.d("abcd", "onComplete: Reading From the database");
-                DocumentSnapshot document = task.getResult();
-                List<String> schedule = (List<String>) document.get("schedule");
-                Log.d("list", "onComplete: Schedule List "+ schedule);
+                    Log.d("abcd", "onComplete: Reading From the database");
+                    DocumentSnapshot document = task.getResult();
+                    List<String> schedule = (List<String>) document.get("schedule");
+                    Log.d("list", "onComplete: Schedule List " + schedule);
+                    Log.d(TAG, "getSchedule: Schedule List Size: " + schedule.size());
 
-        });
+                    for (int i = 0; i < schedule.size(); i++) {
+
+                        String[] Date = schedule.get(i).split("/");
+//                    Log.d(TAG, "getSchedule: Size of date array is:" + Date.length);
+                        String month = Date[0];
+                        String date = Date[1];
+                        String Year = Date[2];
+
+                        Log.d(TAG, "getSchedule: Date: "
+                                + date + " Month: " + month + " Year: " + Year);
+                    }
+                });
+//        Log.d(TAG, "getSchedule: New List size: "+ mSchedule.size());
+////        + "\n" +"List first Element:" + mSchedule.get(1));
+//        return mSchedule;
+
     }
+}
 
 
-//    private  List<String> getSchedule(String uid) {
-//        List<String> schedule = Arrays.asList();
-//        DocumentReference df = fStore.collection("Users").document(uid);
+//   private  List<String> getSchedule(String uid) {
+//              List<String> mSchedule = new ArrayList<>();
+//        DocumentReference df = fStore.collection("Users").document(userID);
 //
-//        df.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//        df.get().addOnCompleteListener(
+//                (@NonNull Task<DocumentSnapshot> task) -> {
 //
-//                Log.d("abcd", "onComplete: Reading From the database");
-//                DocumentSnapshot document = task.getResult();
-//                List<String> schedule = (List<String>) document.get("schedule");
-//                Log.d("list", "onComplete: Schedule List "+ schedule);
-//            }
-//        });
-//        Log.d(TAG, "getSchedule: List size" + schedule.);
-//        return schedule;
+//                    Log.d("abcd", "onComplete: Reading From the database");
+//                    DocumentSnapshot document = task.getResult();
+//                    List<String> schedule = (List<String>) document.get("schedule");
+//                    Log.d("list", "onComplete: Schedule List "+ schedule);
+//                    Log.d(TAG, "getSchedule: Schedule List Size: " + schedule.size());
+//
+//                    for( int i=0; i < schedule.size(); i++) {
+//                        Log.d(TAG, "getSchedule: In the for Loop");
+//                        mSchedule.add(schedule.get(i));
+//                    }
+//
+//                    Log.d(TAG, "getSchedule: Size of new String: "+ mSchedule.size());
+//                });
+//        Log.d(TAG, "getSchedule: Size of new String2: "+ mSchedule.size());
 //
 //    }
-}
+//}
